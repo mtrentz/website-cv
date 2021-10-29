964bcdd33c6c5e099aa2e45f9e42c72465cdf8e6
mateus trentz <mateus.trentz@gmail.com>
2021-10-29 18:52:03 -0300

    added company and university logo

--- src/components/bodies/ContentBodyEducation.js
+++ src/components/bodies/ContentBodyEducation.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import aang from '../../images/aang.png'
+import ufrgs from '../../images/ufrgs.png'
+import maxiquim from '../../images/maxiquim.png'
 
 
 // This card is kinda unique so I wont try to make into a customizable component
 const ContentBodyEducation = () => {
     return (
-        <div className="bg-white w-full h-full flex flex-col justify-center">
+        <div className="bg-white w-full h-full flex flex-col justify-center rounded-md overflow-hidden shadow-lg">
             
             {/* Top Part*/}
-            <div className="bg-gray-300 flex-1 flex flex-row">
+            <div className=" flex-1 flex flex-row">
                 {/* Left Part */}
-                <div className="bg-yellow-500 w-1/5 min-w-min flex items-center justify-center">
+                <div className=" w-1/5 px-4 min-w-min flex items-center justify-center">
                     {/* Container for rounded image (cropped) */}
                     <div className="w-32 h-32 overflow-hidden rounded-full">
                         <img src={aang} alt="Profile Pic" className="h-36 pb-3"/>
@@ -18,9 +20,9 @@ const ContentBodyEducation = () => {
                 </div>
 
                 {/* Right Part */}
-                <div className="flex flex-col flex-1 justify-center lg:mx-10">
-                    <p className="text-lg font-medium text-center">My Full Name</p>
-                    <p className="bg-yellow-700 flex-0 text-center text-sm">
+                <div className="flex flex-col flex-1 justify-center px-4 md:mx-10">
+                    <p className="text-lg font-medium pl-6 pb-2 text-left">My Full Name</p>
+                    <p className=" flex-0 text-left text-sm">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, vero natus sunt doloribus facilis impedit accusantium nisi libero iste reprehenderit cum eveniet ipsa tempora laborum repellat totam quisquam in tempore.
                     </p>
                 </div>
@@ -28,23 +30,44 @@ const ContentBodyEducation = () => {
 
             {/* Separator */}
             <div className="bg white h-5 flex flex-col justify-center items-center">
-                <div className="bg-red-500 w-5/6 h-[1px]"></div>
+                <div className="bg-gray-300 w-5/6 h-[1px]"></div>
             </div>
 
             {/* Bottom Part*/}
-            <div className="bg-red-400 flex flex-row flex-1 justify-around">
+            <div className=" flex flex-row flex-1 justify-around">
                 {/* Left Part */}
-                <div className="bg-gray-500 flex-1">
-                    <span>abcdefg</span>
+                <div className=" flex-1 flex items-center justify-center gap-2">
+                    {/* Logo Part */}
+                    <div className="w-24 h-24 overflow-hidden rounded-full bg-white border">
+                        <img src={ufrgs} alt="Profile Pic" className=""/>
+                    </div>
+
+                    {/* Text Part */}
+                    <div>
+                        <p className="font-semibold text-center">My University</p>
+                        <p className="flex-0 text-center text-sm"> My Graduation Course </p>
+                        <p className="flex-0 text-center text-sm"> 1720 - 1725 </p>
+                    </div>
                 </div>
                 
                 {/* Separator */}
-                <div className="bg-white w-5 flex-none flex flex-col justify-center pl-3">
-                    <div className="bg-red-500 h-3/4 w-[0.1em] transform rotate-[5deg]"></div>
+                <div className=" w-5 flex-none flex flex-col justify-center pl-3">
+                    <div className="bg-gray-300 h-3/4 w-[1px] transform rotate-[5deg]"></div>
                 </div>
 
                 {/* Right Part */}
-                <div className="bg-gray-900 flex-1">
+                <div className="bg-white flex-1 flex items-center justify-center gap-3">
+                    {/* Logo Part */}
+                    <div className="w-32 h-24 overflow-hidden rounded-2xl  border">
+                        <img src={maxiquim} alt="Profile Pic" className=""/>
+                    </div>
+
+                    {/* Text Part */}
+                    <div>
+                        <p className="font-semibold text-center">Company Name</p>
+                        <p className="flex-0 text-center text-sm"> My Job Title </p>
+                        <p className="flex-0 text-center text-sm"> 1725 - present </p>
+                    </div>
                 </div>
             </div>
 
