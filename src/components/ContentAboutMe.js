4fd7d7e9350edcc44b3149d9d1e9e2ab4da38b72
mateus trentz <mateus.trentz@gmail.com>
2021-10-30 00:35:21 -0300

    content navbar selection color click

--- src/components/ContentAboutMe.js
+++ src/components/ContentAboutMe.js
@@ -2,23 +2,38 @@ import React from 'react'
 import ContentTitle from './ContentTitle'
 import ContentNavBar from './ContentNavBar'
 import ContentBodyEducation from './bodies/ContentBodyEducation'
+import { useState } from 'react'
 
 const ContentAboutMe = () => {
 
-    const navbarLinks = [
+    const [navbarLinks, setNavbarLinks] = useState([
         {
+            id: 1,
             name: 'Education',
-            link: '#education'
+            link: '#education',
+            selected: true
         },
         {
+            id: 2,
             name: 'Tech Skills',
-            link: '#tech-skills'
+            link: '#tech-skills',
+            selected: false
         },
         {
+            id: 3,
             name: 'Miscellaneous',
-            link: '#miscellaneous'
+            link: '#miscellaneous',
+            selected: false
         }
-    ]
+    ])
+
+    const handleClick = (id) => {
+        // If clicked, set that one to true, all other ones to false
+        const updatedLinks = navbarLinks.map(link => 
+            link.id === id ? {...link, selected:true} : {...link, selected:false}
+            )
+        setNavbarLinks(updatedLinks)
+    }
 
     return (
         <div className="bg-terciary flex flex-col h-full w-full">
@@ -26,7 +41,7 @@ const ContentAboutMe = () => {
             <ContentTitle title="About Me" />
 
             {/* ContentNav */}
-            <ContentNavBar navbarLinks={navbarLinks}/>
+            <ContentNavBar navbarLinks={navbarLinks} handleClick={handleClick}/>
 
             {/* Content Body */}
             <div className="flex flex-grow items-center justify-center py-5 px-8">
--- src/components/ContentNavBar.js
+++ src/components/ContentNavBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ContentNavBar = ( { navbarLinks } ) => {
+const ContentNavBar = ( { navbarLinks, handleClick } ) => {
 
     return (
 
         <div className="h-8 pl-16 flex flex-row gap-3 items-center">
             {navbarLinks.map( (links) => (
                 <a href={links.link}>
-                    <div className="bg-accent h-2 w-16 rounded-full"></div>
+                    <div className={`${ links.selected ? "bg-secondary" : "bg-accent"} h-2 w-16 rounded-full`} onClick={() => handleClick(links.id)}></div>
                 </a>
             ))}
         </div>
