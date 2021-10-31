d4edd097ff56e8083312db06bdb34a432cedbf01
mateus trentz <mateus.trentz@gmail.com>
2021-10-31 15:05:00 -0300

    added routes to sidebar

--- src/components/MenuSubSection.js
+++ src/components/MenuSubSection.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MenuSubSection = ( { sectionTitle, sectionIcon, sectionHref } ) => {
     return (
-        <a href={sectionHref} className=" flex flex-row flex-nowrap ml-10 my-2 items-center text-md">
+        <Link to={sectionHref} className=" flex flex-row flex-nowrap ml-10 my-2 items-center text-md">
             { sectionIcon }
             <span className="ml-1"> { sectionTitle } </span>
-        </a>
+        </Link>
     )
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MenuMainSection from './MenuMainSection'
 import MenuSubSection from './MenuSubSection'
 import { AiOutlineUser } from 'react-icons/ai'
@@ -12,43 +13,47 @@ const SideBar = () => {
     return (
         <div>
             <nav className="xl:w-64 2xl:w-72 bg-white shadow-2xl">
-                <div className="flex flex-col h-screen">
-                    {/* logo */}
-                    <div className="h-[25%] flex-grow-0 flex items-center">
-                        <a href="#" className="block m-auto">
-                            <svg className="w-24 h-24 md:w-26 md:h-26 lg:w-30 lg:h-30 xl:h-32 xl:w-32 2xl:h-40 2xl:w-40  rounded-lg md:rounded-xl lg: opacity-90" viewBox="0 0 143 143" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#67839C" fill-opacity=".9" d="M0 0h143v143H0z"/><path d="M38.908 38.55H22.522l16.386 81.791v-81.79ZM71.679 38.55v65.542L104.45 38.55H71.952v-.273h-.273V22.165h49.156V38.55l-32.77 65.542h32.77v16.385H55.43V38.55h16.249Z" fill="#fff" stroke="#fff" stroke-width="2"/></svg>
-                        </a>
-                    </div>
-
-                    {/* menu */}
-                    {/* TODO isso aqui deveria sharear uma lista junto com o ContentNavBar */}
-                    <div className="flex-grow mr-2">
-                        < MenuMainSection sectionTitle="About Me" sectionIcon={<AiOutlineUser />} sectionHref="#"/>
-                            < MenuSubSection sectionTitle="Education" sectionIcon={<HiOutlineAcademicCap />} sectionHref="/education" />
-                            < MenuSubSection sectionTitle="Tech Skills" sectionIcon={<HiOutlineTerminal />} sectionHref="/tech-skills" />
-                            < MenuSubSection sectionTitle="Miscellaneous" sectionIcon={<HiOutlinePuzzle />} sectionHref="#" />
-                        < MenuMainSection sectionTitle="Projects" sectionIcon={ <HiOutlineSparkles /> } sectionHref="#"/>
-                            < MenuSubSection sectionTitle="Coding" sectionIcon={<HiOutlineCode />} sectionHref="#" />
-                            < MenuSubSection sectionTitle="Data Vizzes" sectionIcon={<HiOutlineChartBar />} sectionHref="#" />
-                            < MenuSubSection sectionTitle="Others" sectionIcon={<HiOutlineLightBulb />} sectionHref="#" />
-                    </div>
-
-                    {/* socials */}
-                    <div className="h-24 flex-grow-0 mb-3">
-                        <p className="mb-2 ml-4 font-medium">Find me on</p>
-                        <div className="flex flex-row justify-center gap-3 items-center">
-                            <a href="#" title="Link to my GitHub">
-                                < FaGithubSquare className="w-12 h-12 text-gray-700"/>
-                            </a>
-                            <a href="#" title="Link to my Tableau Public">
-                                < IoLogoTableau className="w-10 h-10 bg-gray-700 text-white rounded-[4px] p-[2px]"/>
-                            </a>
-                            <a href="#" title="Link to my LinkedIn">
-                                < IoLogoLinkedin className="w-12 h-12 text-gray-700"/>
+                <Router>
+                    <div className="flex flex-col h-screen">
+                        {/* logo */}
+                        <div className="h-[25%] flex-grow-0 flex items-center">
+                            <a href="#" className="block m-auto">
+                                <svg className="w-24 h-24 md:w-26 md:h-26 lg:w-30 lg:h-30 xl:h-32 xl:w-32 2xl:h-40 2xl:w-40  rounded-lg md:rounded-xl lg: opacity-90" viewBox="0 0 143 143" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#67839C" fill-opacity=".9" d="M0 0h143v143H0z"/><path d="M38.908 38.55H22.522l16.386 81.791v-81.79ZM71.679 38.55v65.542L104.45 38.55H71.952v-.273h-.273V22.165h49.156V38.55l-32.77 65.542h32.77v16.385H55.43V38.55h16.249Z" fill="#fff" stroke="#fff" stroke-width="2"/></svg>
                             </a>
                         </div>
+
+                        {/* menu */}
+                        {/* TODO isso aqui deveria sharear uma lista junto com o ContentNavBar */}
+                        <Switch>
+                            <div className="flex-grow mr-2">
+                                < MenuMainSection sectionTitle="About Me" sectionIcon={<AiOutlineUser />} sectionHref="#"/>
+                                    < MenuSubSection sectionTitle="Education" sectionIcon={<HiOutlineAcademicCap />} sectionHref="/education" />
+                                    < MenuSubSection sectionTitle="Tech Skills" sectionIcon={<HiOutlineTerminal />} sectionHref="/tech-skills" />
+                                    < MenuSubSection sectionTitle="Miscellaneous" sectionIcon={<HiOutlinePuzzle />} sectionHref="#" />
+                                < MenuMainSection sectionTitle="Projects" sectionIcon={ <HiOutlineSparkles /> } sectionHref="#"/>
+                                    < MenuSubSection sectionTitle="Coding" sectionIcon={<HiOutlineCode />} sectionHref="#" />
+                                    < MenuSubSection sectionTitle="Data Vizzes" sectionIcon={<HiOutlineChartBar />} sectionHref="#" />
+                                    < MenuSubSection sectionTitle="Others" sectionIcon={<HiOutlineLightBulb />} sectionHref="#" />
+                            </div>
+                        </Switch>
+
+                        {/* socials */}
+                        <div className="h-24 flex-grow-0 mb-3">
+                            <p className="mb-2 ml-4 font-medium">Find me on</p>
+                            <div className="flex flex-row justify-center gap-3 items-center">
+                                <a href="#" title="Link to my GitHub">
+                                    < FaGithubSquare className="w-12 h-12 text-gray-700"/>
+                                </a>
+                                <a href="#" title="Link to my Tableau Public">
+                                    < IoLogoTableau className="w-10 h-10 bg-gray-700 text-white rounded-[4px] p-[2px]"/>
+                                </a>
+                                <a href="#" title="Link to my LinkedIn">
+                                    < IoLogoLinkedin className="w-12 h-12 text-gray-700"/>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </Router>
             </nav>
 
             <nav>
